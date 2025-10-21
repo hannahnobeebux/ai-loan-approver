b2471875dae37135a283d7271e035cde589ea3c4
kersh2000 <bradleykershaw333@gmail.com>
2025-10-21 10:59:26 +0100

    Refine data source and changelogs

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+# CHANGELOG.md
+
+This file documents the changes of the project through additions, removals, changes, bugs, and fixes. Changes are documented through branches, user stories, and dates.
+
+## [Unreleased]
+
+### Features
+
+- Added a local source of data to train the machine learning model for credit cirsk/score.
+
+## Template
+## [0.0] - Date
+
+### Additions
+
+- x
+
+### Removals
+
+- x
+
+### Changes
+
+- x
+
+### Bugs
+
+- x
+
+### Fixes
+
+- x
--- training-data.py
+++ training-data.py
@@ -1,27 +1,17 @@
 import pandas as pd
 
 def load_training_data(file_path):
-    # pd.set_option('display.max_columns', None)
-    # data = pd.read_csv(file_path, 
-    #                    names=["Status of existing checking account", "Duration", 
-    #                           "Credit history", "Purpose", "Credit amount", "Savings account/bonds", 
-    #                           "Present employment since", "Installment rate in percentage of disposable income", 
-    #                           "Personal status", "Other debtors/guarantors", "Residence since", "Property", "Age", 
-    #                           "Other installment plans", "Housing", "Number of existing credits at this bank", "Job", 
-    #                           "Number of people being liable to provide maintenance for", "Telephone", "Foreign worker", 
-    #                           "Good/Bad"],
-    #                     sep=' ', 
-    #                     header=None)
-    # print(data.head())
-    # return data
-    data = pd.read_csv(file_path)
-    print(data.head())
-    return data
+    # Read data source into pandas dataframe (df)
+    df = pd.read_csv(file_path)
+    # Print head of df
+    print(df.head())
+    return df
 
 
 if __name__ == "__main__":
-    # Download latest version
+    # Configure pandas
+    pd.set_option('display.max_columns', None)
+    # Specify data source
     file_path = "credit_risk_dataset.csv"
-    print("Path to dataset files:", file_path)
-    # file_path = "german-credit-data/german.data"
-    load_training_data(file_path)
+    # Load training data into model
+    data = load_training_data(file_path)
