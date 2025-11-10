bf3012adc2bfef5ce8b5bde1a940f04cefd4d452
kersh2000 <bradleykershaw333@gmail.com>
2025-11-10 12:58:46 +0000

    Add comments for better clarity

--- gui.py
+++ gui.py
@@ -4,9 +4,11 @@ from tkinter import messagebox
 from gui_tests import Tests
 from logic import LogicComponent
 
+# A class used to represent the frontend of the application
 class Gui:
 
     def __init__(self):
+        # Components to contain the GUI instances
         self.components = {
             "loan_amnt": None,
             "term": None,
@@ -33,12 +35,14 @@ class Gui:
         self.create_window()
 
     def create_window(self):
+        # Create a centered window
         self.window.title("AI Loan Manager - Prototype")
         self.centre_sized_window(800, 500)
         self.add_widgets()
 
     def centre_sized_window(self, width, height):
         self.window.update_idletasks()
+        # Centre the window with given dimensions
         screen_width = self.window.winfo_screenwidth()
         screen_height = self.window.winfo_screenheight()
         x = (screen_width - width) // 2
@@ -46,17 +50,23 @@ class Gui:
         self.window.geometry(f"{width}x{height}+{x}+{y}")
 
     def add_widgets(self):
+        # Add the application page (future proof for more pages)
         self.add_application_page()
 
     def add_application_page(self):
+        # Add title
         title = tk.Label(text='Loan Application Page')
         title.pack()
 
+        # Create a frame for the form
         frame = tk.Frame(master=self.window,borderwidth=2)
 
+        # Number of entires per row in the form
         columns = 4
         counter = 0
+        # Produce an entry witha  label for all components
         for key in self.components:
+            # Frame for label + entry
             f = tk.Frame(master=frame)
             row = counter//columns
             col = counter%columns
@@ -69,20 +79,25 @@ class Gui:
         
         frame.pack()
 
+        # Add submit button for the form
         submit = tk.Button(master=self.window, text="Submit Application", command=self.submit_application)
         submit.pack()
 
     def submit_application(self):
         application_values = {}
+        # Define string entreis
         string_vals = ["home_ownership", "purpose", "employment_type"]
         for key, value in self.components.items():
+            # Obtain entry value
             component_val = value.get()
+            # Handle missing entries
             if not component_val:
                 messagebox.showerror("Validation Error", f"Value '{key}' must be supplied.")
                 return 0
             if key in string_vals:
                 application_values[key] = component_val
             else:
+                # Handle non-string entries (if not string, assume float)
                 try:
                     application_values[key] = float(component_val)
                 except ValueError:
@@ -90,15 +105,21 @@ class Gui:
                     messagebox.showerror("Validation Error", f"Value '{key}' must be numeric.")
                     return 0
         print(application_values)
+        # Call and obtain decision from the Logic component
         decision = self.logic.process_application(application_values)
         response = decision.outcome
         ml_score = round(decision.ml_score, 0)
         reasons = decision.reasons
         symbol_score = decision.symbolic_score
         output = "Approved" if response == "APPROVE" else "Denied"
-        messagebox.showinfo("Loan App Outcome", f"Loan {output}!\nML Score: {ml_score}\nSymbolic Score: {symbol_score}\nReasons: {reasons}")
+        # Output the decision
+        messagebox.showinfo("Loan App Outcome", f"Loan {output}!\n \
+                            ML Score: {ml_score}\n \
+                            Symbolic Score: {symbol_score}\n \
+                            Reasons: {reasons}")
 
     def handle_tests(self):
+        # Create test buttons to load applications quickly
         tests = [
             self.tests.test_case_1,
             self.tests.test_case_2,
@@ -119,11 +140,15 @@ class Gui:
         frame.pack()
 
     def load_test_data(self, test):
+        # Load test values into the entries depending on the test button pressed
         for key, value in self.components.items():
             value.delete(0, tk.END)
             value.insert(0, test[key])
 
 if __name__ == '__main__':
+    # Create the GUI
     frontend = Gui()
+    # Create the test buttons
     frontend.handle_tests()
+    # Start the GUI application
     frontend.window.mainloop()
--- model.py
+++ model.py
@@ -9,17 +9,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.calibration import CalibratedClassifierCV
 import logging
 
-# A class used to represent a machine-learning model using Logistic Regression
+# A class used to represent the ML model to produce a credit risk score
 class Model:
 
     def __init__(self):
         pd.set_option('display.max_columns', None)
+        # Initialisation of class attributes to be used between methods
         self.data = []
         self.numeric_cols = []
         self.categorical_cols = []
         self.X_train, self.y_train, self.X_cal, self.y_cal, self.X_test, self.y_test = (None,)*6
         self.pipeline, self.model = (None,)*2
 
+        # Initialise logger with certain format
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(
             filename="logs/model.log",
@@ -34,9 +36,11 @@ class Model:
 
     def load_data(self, train_data_filepath, numeric_cols, categorical_cols, nrows):
         self.logger.info("Loading Data...")
+        # Define the numeric and categorical columns for later preprocessing
         self.numeric_cols = numeric_cols
         self.categorical_cols = categorical_cols
         cols = numeric_cols + categorical_cols
+        # Attempt to read the csv of the chosen number of rows and columns
         try:
             self.data = pd.read_csv(train_data_filepath, nrows=nrows, usecols=cols)
             self.logger.info(f"Successfully Loaded Data:\n(first 5 rows)\n{self.data.head(5)}")
@@ -54,7 +58,7 @@ class Model:
             if col in self.data.columns:
                 self.data = self.data.dropna(subset=[col])
         
-        # Removing the word "months" from the term column and converting to integer
+        # Removing the word "months" from the term column and convert to integer
         if "term" in self.data.columns:
             self.data["term"] = self.data["term"].astype(str).str.replace(" months", "", regex=False).astype(int)
 
@@ -69,19 +73,17 @@ class Model:
             self.data["emp_length"] = emp
 
         # If interest rate is null, replace with mean interest rate
-        # if "int_rate" in self.data.columns:
-        #     self.data["int_rate"] = self.data["int_rate"].replace(float("NaN"), self.data["int_rate"].mean())
         apr_mean = self.data["int_rate"].mean()
         if "int_rate" in self.data.columns:
             self.data["int_rate"] = self.data["int_rate"].fillna(apr_mean)
 
-        # Handling outliers by capping and log transforming positively skewed data
- 
+        # Handle outliers
         for col in ["loan_amnt", "annual_inc", "revol_bal"]:
             if col in self.data.columns:
                 upper = self.data[col].quantile(0.99)
                 self.data[col] = np.clip(self.data[col], 0, upper)
         
+        # Transform Columns
         if all(c in self.data.columns for c in ["loan_amnt", "annual_inc"]):
             self.data["loan_to_income"] = self.data["loan_amnt"] / (self.data["annual_inc"] + 1)
 
@@ -103,28 +105,43 @@ class Model:
 
     def process_data(self):
         self.logger.info("Processing Data...")
+
+        # Identify label
         y = self.data["repay_fail"]
-        # Obtain data into 2 buckets: training & calibration + testing
+        
+        # Split data into 3 categories: training, tetsing, calibrating
         self.X_train, X_temp, self.y_train, y_temp = train_test_split(self.data.drop(columns="repay_fail"), y, 
                                                             test_size=0.4, random_state=42, stratify=y)
         self.X_cal, self.X_test, self.y_cal, self.y_test = train_test_split(X_temp, y_temp, 
                                                             test_size=0.5, random_state=42, stratify=y_temp)
+        # Remove label
         if "repay_fail" in self.numeric_cols:
             self.numeric_cols.remove("repay_fail")
+
+        # Form pipeline for preprocessing different data types
         preprocessor = ColumnTransformer([
             ('num', StandardScaler(), self.numeric_cols),
             ('cat', OneHotEncoder(handle_unknown='ignore'), self.categorical_cols),
         ])
+
+        # Add ML model
         lr = LogisticRegression(max_iter=1000, C=5.0, solver="lbfgs", class_weight="balanced")
         self.pipeline = Pipeline([("prep", preprocessor), ("model", lr)])
+
+        # Process data
         data = preprocessor.fit_transform(self.X_train, self.y_train)
         self.logger.info(f"First 5 Rows of pre-processed data:\n{data[0:5]}")
 
     def train_model(self):
         self.logger.info("Training Model...")
+        # Handle data cleaning and processing
         self.clean_data()
         self.process_data()
+
+        # Preprocess and fit the data against the model
         self.pipeline.fit(self.X_train, self.y_train)
+
+        # Calibrate the model
         self.model = CalibratedClassifierCV(
             estimator=self.pipeline,
             cv=5,
@@ -135,19 +152,22 @@ class Model:
 
     def test_model(self):
         self.logger.info("Testing Model...")
+        # Produce risk probabilities (removing the label) and produce accuracy scores
         y_proba = self.model.predict_proba(self.X_test)[:,1]
         self.logger.info(f"ROC-AUC:\n{roc_auc_score(self.y_test, y_proba)}")
         self.logger.info(f"PR_AUC:\n{average_precision_score(self.y_test, y_proba)}")
 
+        # Produce confusion matrices and classification reports for different cutoff values
         cutoffs = [0.05, 0.08, 0.12, 0.16, 0.20, 0.25]
         for cutoff in cutoffs:
-            y_pred_policy = (y_proba >= cutoff).astype(int)  # 1 = predict default (reject)
+            y_pred_policy = (y_proba >= cutoff).astype(int)
             self.logger.info(f"Confusion Matrix (reject=1 at cutoff {cutoff}):\n"
                              f"{confusion_matrix(self.y_test, y_pred_policy)}")
             self.logger.info(f"Classification Report\n{classification_report(self.y_test, y_pred_policy, digits=3)}")
         self.logger.info("Model Tested Successfully!")
 
     def process_application(self, X):
+        # First produce the transformation columns
         if all(c in X.columns for c in ["loan_amnt", "annual_inc"]):
             X["loan_to_income"] = X["loan_amnt"] / (X["annual_inc"] + 1)
 
@@ -156,7 +176,11 @@ class Model:
 
         if all(c in X.columns for c in ["open_acc", "revol_bal"]):
             X["revol_per_acc"] = X["revol_bal"] / (X["open_acc"] + 1)
+        
+        # Obtain risk probability
         probability = self.model.predict_proba(X)[:,1][0]
+
+        # Transform risk score into credit risk score and return
         risk_score = 1000 - (probability * 1000)
         print(f"Predicted Application Score = {risk_score}")
         return risk_score
@@ -164,6 +188,7 @@ class Model:
 
 if __name__ == '__main__':
     model = Model()
+    # Define categories to be chosen from the csv
     numeric = [
         "loan_amnt", "int_rate",
         "annual_inc", "delinq_2yrs", "open_acc", "pub_rec",
@@ -171,15 +196,17 @@ if __name__ == '__main__':
     ]
     categorical = ["emp_length", "home_ownership", "purpose"]
 
+    # Load a specific csv with chosen columns and rows
     model.load_data("loan_data.csv", numeric, categorical, 10000)
     model.train_model()
     model.test_model()
 
+    # Test the model with a sample application
     sample_applicant = pd.DataFrame([{
         'loan_amnt': 6000.0,                # requested loan amount
         'term': 36,                         # loan term in months
         'int_rate': 12.5,                   # offered APR %
-        'emp_length': 6,                    # years employed (or bucketed int if that's how you encoded)
+        'emp_length': 6,                    # years employed
         'home_ownership': 'RENT',           # home ownership category
         'annual_inc': 55000.0,              # yearly income
         'purpose': 'debt_consolidation',    # loan purpose category
@@ -190,5 +217,6 @@ if __name__ == '__main__':
 
     }])
 
+    # Output the sample applications score
     score = model.process_application(sample_applicant)
     model.logger.info(f"Predicted Application Score = {score}")
